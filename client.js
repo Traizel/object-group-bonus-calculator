5c73185e17d6d1cdac30713840917541b7dbf3c8
Tre Olson <treolson@live.com>
2021-01-05 16:24:30 -0600

    finish goals

--- client.js
+++ client.js
@@ -40,4 +40,76 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+
+
+
 console.log( employees );
+
+for (const employee of employees) {
+  console.log( 'Checking bonus for', employee.name );
+  bonusCalculator(employee);
+}
+
+function bonusCalculator( employee ) {
+let bonus = 0;
+
+function reviewRating() {
+  if (employee.reviewRating === 5) {
+    bonus += 10;
+  }
+  if (employee.reviewRating === 4) {
+    bonus += 6;
+  }
+  if (employee.reviewRating === 3) {
+    bonus += 4;
+  } else {
+    bonus += 0;
+  }
+}
+
+function employeeNumber() {
+  if (employee.employeeNumber.length >= 4 && employee.reviewRating > 2) {
+    bonus += 5;
+    console.log( employee.name + ' gets an extra 5% bonus!' );
+  }
+}
+
+function annualSalary() {
+  if (employee.annualSalary > 65000) {
+    bonus -= 1;
+    console.log( employee.name + " gets a 1% deduction cuz they're too rich" );
+  }
+}
+  function highLow() {
+    if (bonus > 13) {
+      bonus = 13;
+    }
+    if (bonus < 0) {
+      bonus = 0;
+    }
+  }
+
+
+  reviewRating(employee);
+  employeeNumber(employee);
+  annualSalary(employee);
+  highLow();
+  let bonusPercent = bonus / 100;
+  console.log(bonusPercent)
+  let totalBonus = bonusPercent * employee.annualSalary;
+  console.log(totalBonus);
+  let totalCompensation = totalBonus + Number(employee.annualSalary);
+  console.log(totalCompensation);
+
+  let answer = {
+    name: employee.name,
+    bonusPercentage: bonusPercent,
+    totalBonus: totalBonus,
+    totalCompensation: totalCompensation
+  };
+  console.log( answer );
+  $("#bonus").append("<li> Name: " + answer.name + " -- Bonus %: " + answer.bonusPercentage + "% -- Total Bonus: $" + answer.totalBonus + " -- Total Compensation: " + answer.totalCompensation + "</li>");
+  
+  return answer;
+}
+
